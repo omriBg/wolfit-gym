e71a82abcf03753a920775543376d540ab565010
omriBg <omri952682@gmail.com>
2025-08-18 08:42:07 +0300

    התאמה מקיפה לכל סוגי המסכים - מובייל, טאבלט, דסקטופ

--- src/SignUpPreferences.js
+++ src/SignUpPreferences.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import './EditUser.css'; // נשתמש באותו עיצוב
+import './SignUpPreferences.css';
 
 
 async function sendRegistrationToServer(userData) {
